75945f098ecab476af589641743e2eac5586c413
Santiago <san@san.com>
2024-02-21 20:54:17 +0100

    testing linker

--- tools/disasm.py
+++ tools/disasm.py
@@ -1,19 +1,23 @@
 #!/usr/bin/python3
+import re
 import sys
 import rabbitizer
 
 def mipsInstrFromWordString(word):
-	rabinstr = rabbitizer.Instruction(eval(f"0x{word.replace(' ', '')}"))
+	rabinstr = rabbitizer.Instruction(eval(f"0x{word}"))
 	rabinstrstr = f"{rabinstr}"
 	return rabinstrstr[:rabinstrstr.find(' ') if ' ' in rabinstrstr else len(rabinstrstr)]
 
 def addInstrToHexDump(inFilePath):
 	with open(inFilePath, 'r') as inFile:
 		for line in inFile.readlines():
-			a = mipsInstrFromWordString(line[10:19])
-			b = mipsInstrFromWordString(line[20:29])
-			c = mipsInstrFromWordString(line[30:39])
-			d = mipsInstrFromWordString(line[40:49])
+			m = re.match(r'\w+: (\w{4} \w{4}) (\w{4} \w{4}) (\w{4} \w{4}) (\w{4} \w{4})', line)
+			if m is None:
+				continue
+			a = mipsInstrFromWordString(m.group(1).replace(' ', ''))
+			b = mipsInstrFromWordString(m.group(2).replace(' ', ''))
+			c = mipsInstrFromWordString(m.group(3).replace(' ', ''))
+			d = mipsInstrFromWordString(m.group(4).replace(' ', ''))
 			print(line[:-1] + f'  {a}{" "*(8-len(a))}{b}{" "*(8-len(b))}{c}{" "*(8-len(c))}{d}')
 
 def disassemble(inFilePath, littleEndian=False):
