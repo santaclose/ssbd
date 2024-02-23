c81a2900ad324de9b402f249a0335104185b0fcf
Santiago <san@san.com>
2024-02-23 23:54:54 +0100

    h

---
+++ include/sys/rldm.h
@@ -0,0 +1,26 @@
+#ifndef SYS_RLDM_H
+#define SYS_RLDM_H
+
+typedef struct RldmFileNode 
+{
+	/* 0x00 */ u32 id;
+	/* 0x04 */ void *addr;
+} RldmFileNode; // size == 8
+
+typedef struct RldmSetup
+{
+	/* 0x00 */ s32 tableRomAddr;
+	/* 0x04 */ u32 tableFileCount;
+	/* 0x08 */ u8 *fileHeap;
+	/* 0x0C */ u32 fileHeapSize;
+	/* 0x10 */ RldmFileNode *statusBuf;
+	/* 0x14 */ s32 statusBufSize;
+	/* 0x18 */ RldmFileNode *forceBuf;
+	/* 0x1C */ s32 forceBufSize;
+} RldmSetup; // size == 0x20
+
+
+u32 rldm_bytes_needed_to_load(s32*);
+s32 rldm_get_file_with_external_heap(s32*, void*);
+
+#endif /* SYS_RLDM_H */
